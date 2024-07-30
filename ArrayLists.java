35d0fcee3a197ae3c15b45e69b5259a2214afeeb
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-07-30 13:32:27 +0530

    Update ArrayLists.java

--- ArrayLists.java
+++ ArrayLists.java
@@ -21,3 +21,28 @@ public static ArrayList<Integer> greater(int arr[]){                    //next g
         ArrayList<Integer> res = greater(arr);
         System.out.println(res);
     }
+
+
+public class arraylist{                                           //Find out if the arraylist has duplicates 
+    public static void main(String args[]){
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        System.out.println(mono(arr));
+    }
+    public static boolean mono(ArrayList<Integer> arr){
+        if(arr.size()==0){
+            return false;
+        }
+        for(int i=0;i<arr.size()-1;i++){
+            for(int j=0;j<arr.size();j++){
+                if(arr.get(i)==arr.get(i+1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

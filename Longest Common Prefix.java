1dd6225c22f09c60e6ab2bfe156b6a0888b58653
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-08-13 13:06:00 +0530

    Create Longest Common Prefix.java

---
+++ Longest Common Prefix.java
@@ -0,0 +1,16 @@
+/* Write a function to find the longest common prefix string amongst an array of strings.
+
+ If there is no common prefix, return an empty string "".*/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix=strs[0];
+        for(int i=0;i<strs.length;i++){
+            while(strs[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
+

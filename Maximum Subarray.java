d7b2dbf7d7fabeeedd45f507eee32bd006024c0f
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-08-13 13:18:47 +0530

    Create Maximum Subarray.java

---
+++ Maximum Subarray.java
@@ -0,0 +1,14 @@
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int curr=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            curr=Math.max(nums[i],curr+nums[i]);
+            max=Math.max(curr,max);
+        }
+        return max;
+    }
+}

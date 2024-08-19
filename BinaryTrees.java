0287de47dc479ad075dbce2ab73bddad52b59c64
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-08-19 15:37:42 +0530

    Update BinaryTrees.java

--- BinaryTrees.java
+++ BinaryTrees.java
@@ -1,52 +1,84 @@
-    public class binarytree {
-        public static class Node{
-            int data;
-            Node left;
-            Node right;
-            public Node(int data){
-                this.data=data;
-                this.left=null;
-                this.right=null;
-            }
-        }
-
-        public static class Tree{
-            static int idx=-1;
-            public static Node build(int nodes[]){                         //build a binary tree
-                idx++;
-                if(nodes[idx]==-1){
-                    return null;
-                }
-                Node newnode = new Node(nodes[idx]);
-                newnode.left=build(nodes);            
-                newnode.right=build(nodes);
-                return newnode;
-            }
-        }
-
-        public static void preorder(Node root){                   //preorder
-            if(root==null){
-                return;
-            }
-            System.out.print(root.data+" ");
-            preorder(root.left);
-            preorder(root.right);
-        }
-
-        public static void postorder(Node root){          //postorder
-            if(root==null){
-                return;
-            }
-            postorder(root.left);
-            postorder(root.right);
-            System.out.print(root.data + " ");
-        }
-
-
-        public static void main(String args[]){
-            int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-            Tree t = new Tree();
-            Node root=t.build(nodes); 
-            postorder(root);
+public class binarytree {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        public Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
         }
     }
+
+    public static class Tree{
+        static int idx=-1;
+        public static Node build(int nodes[]){                         //build a binary tree
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newnode = new Node(nodes[idx]);
+            newnode.left=build(nodes);            
+            newnode.right=build(nodes);
+            return newnode;
+        }
+    }
+
+    public static void preorder(Node root){                   //preorder
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void postorder(Node root){          //postorder
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+    
+    public static void inorder(Node root){                   //Inorder Traversal
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void levelorder(Node root){                       //LevelOrder Traversal
+        if(root==null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+    
+        while(!q.isEmpty()){
+            Node curr=q.remove();
+            if(curr==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.print(curr.data + " ");
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+}

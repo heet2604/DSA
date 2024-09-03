fa61e0ba1daf5e8d1e5cd93ce6b82054de87b818
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-09-03 18:55:14 +0530

    Update BinaryTrees.java

--- BinaryTrees.java
+++ BinaryTrees.java
@@ -81,4 +81,24 @@ public class binarytree {
             }
         }
     }
+
+    public static int height(Node root){           //height of a tree(nodes)
+        if(root==null){
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        int h = Math.max(lh,rh)+1;
+        return h;
+    }
+
+    public static int count(Node root){          //total no of nodes
+        if(root==null){
+            return 0;
+        }
+        int lc=count(root.left);
+        int rc=count(root.right);
+        int total=lc+rc+1;
+        return total;
+    }
 }

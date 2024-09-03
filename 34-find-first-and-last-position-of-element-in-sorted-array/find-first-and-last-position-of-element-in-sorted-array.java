7c81e040bc51cdb9ee1df24bd79693f4e1ccde6a
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-09-03 20:04:54 +0530

    Update find-first-and-last-position-of-element-in-sorted-array.java

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -7,8 +7,8 @@ class Solution {
             int mid=(l+r)/2;
             if(nums[mid]==target){
                 result[0]=mid;
-                r=mid-1;
-            }
+                r=mid-1;                        //search in the first half of the array
+            } 
             else if(nums[mid]<target){
                 l=mid+1;
             }
@@ -16,13 +16,13 @@ class Solution {
                 r=mid-1;
             }
         }
-        l=0;
+        l=0;                             //reset the index of l and r
         r=nums.length-1;
         while(l<=r){
             int mid=(l+r)/2;
             if(nums[mid]==target){
                 result[1]=mid;
-                l=mid+1;
+                l=mid+1;                    //search in the second half of the array
             }
             else if(nums[mid]<target){
                 l=mid+1;

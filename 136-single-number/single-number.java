4ac73ca9dd3fb64a3a529f99f00c9845e98f3e36
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-09-05 10:10:39 +0530

    Time: 7 ms (31.34%) | Memory: 46.1 MB (49.51%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
+        if (nums.length == 1) {
+            return nums[0]; // If there's only one element, return it.
         }
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+
+        Arrays.sort(nums); // Sort the array
+
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] != nums[i + 1]) { 
+                return nums[i]; // If current element is not equal to the next one, it is the single number.
             }
-            if(count==1){
-                return nums[i];
-            }
+            i += 2; // Skip the next element as they are a pair
         }
-        return 0;
+    // If the single number is the last element in the sorted array
+        return nums[nums.length - 1];
     }
 }

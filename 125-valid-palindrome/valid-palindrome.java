11a2e9cee822184d49103373e2d01dc29b470fef
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-09-08 19:11:45 +0530

    Time: 14 ms (34.31%) | Memory: 44.9 MB (31.44%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution{
+    public static boolean isPalindrome(String s){
+        s=s.toLowerCase();
+        s = s.replaceAll("[\\W,_\":]", "");
+        int n = s.length();
+        if(s.isEmpty()){
+            return true;
+        }
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}

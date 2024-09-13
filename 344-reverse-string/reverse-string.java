06a84352457e6029ad1b31fca1f53115b0c38504
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-09-13 18:34:09 +0530

    Time: 4 ms (7.88%) | Memory: 49.3 MB (28.38%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,12 +1,23 @@
 class Solution {
     public void reverseString(char[] s) {          //s={"h","e","l","l","o"}
-        Stack <Character> st = new Stack<>();
-        for(char c : s){
-            st.push(c);                   //st = {h,e,l,l,o}
+    //     Stack <Character> st = new Stack<>();
+    //     for(char c : s){
+    //         st.push(c);                   //st = {h,e,l,l,o}
+    //     }
+    //     for(int i=0;i<s.length;i++){
+    //         s[i]=st.pop();          //pop the element from the stack and fill the string s with it
+    //     }                              //s={o,e,l,l,o}  {o,l,l,l,o}  {o,l,l,l,o} {o,l,l,e,o} {o,l,l,e,h}(print this)
+    //     System.out.print(s);
+    // }
+        int l=0;
+        int h = s.length-1;
+        while(l<h){
+            char temp = s[l];
+            s[l]=s[h];
+            s[h]=temp;
+            l++;
+            h--;
         }
-        for(int i=0;i<s.length;i++){
-            s[i]=st.pop();          //pop the element from the stack and fill the string s with it
-        }                              //s={o,e,l,l,o}  {o,l,l,l,o}  {o,l,l,l,o} {o,l,l,e,o} {o,l,l,e,h}(print this)
         System.out.print(s);
     }
 }

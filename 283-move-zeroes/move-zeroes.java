917e8e2d8037d1139c4a2f18ec17d61cbd556f52
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-09-28 11:40:25 +0530

    Time: 1 ms (99.89%) | Memory: 45.7 MB (67.17%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,16 +3,24 @@ class Solution {
         if(nums.length==1 && nums[0]==0){
             return;
         }
-        
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==0){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
+        // for(int i=0;i<nums.length-1;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]==0){
+        //             int temp=nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //         }
+        //     }
+        // }
+        int nonzero=0;
+        for(int i=0;i<nums.length;i++){         //bring all the non zero to the start of the array
+            if(nums[i]!=0){
+                nums[nonzero] = nums[i];
+                nonzero++;
             }
         }
-        
+        for(int i=nonzero;i<nums.length;i++){    //fill all the remaining index with 0
+            nums[i]=0;
+        }
     }
 }

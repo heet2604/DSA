9d061b32933910f597a5ea89210dc8951dd5ebb1
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-10-08 09:42:50 +0530

    Time: 5 ms (22.28%) | Memory: 43.3 MB (70.49%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int res[]=new int[nums1.length];
+        int result[] = new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            int nextgreater=-1;         
-            boolean found=false;
+            int nextgreater = -1;
+            boolean found = false;
             for(int j=0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j]){
-                    found=true;               //check if the number is present in both the arrays to check for the greater element 
+                    found = true;
                 }
                 if(found==true && nums2[j]>nums1[i]){
-                    nextgreater=nums2[j];     //if greater is present change its value
-                    break;                 //if the number is found no need to check any further
-                }  
+                    nextgreater = nums2[j];
+                    break;
+                }
             }
-            res[i]=nextgreater;                //add the greater element in the new array or add -1 if not 
+            result[i]=nextgreater;
         }
-        return res;   
+        return result;
     }
 }

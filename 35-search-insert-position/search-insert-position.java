23a921a9aee58d178942c765960a8d8f1a195af9
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2025-01-26 10:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (39.35%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int h = nums.length-1;
+        int l = 0 ; 
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if (nums[mid]>target){
+                h = mid-1;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+        }
+        return l;
+    }   
+}

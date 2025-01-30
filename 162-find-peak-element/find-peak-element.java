25667ce7e5437e0f5143d4b91af67e566d567dc6
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2025-01-30 07:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (71.68%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        //int max = nums[0] ;
+        int max = nums[0] ;
         if(nums.length==1){             //single element array
             return 0;
         }
@@ -10,26 +10,26 @@ class Solution {
         if(nums[nums.length-1]>nums[nums.length-2]){ //last element peak
             return nums.length-1;
         }
-        // for(int i=1;i<nums.length-1;i++){
-        //     if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-        //         max=Math.max(max,nums[i]);
-        //         return i;
-        //     }
-        // }
-        int l = 1;
-        int h = nums.length-2;
-        while(l<=h){
-            int mid = l + (h-l)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-                return mid;
-            }
-            if(nums[mid]>nums[mid-1]){
-                l = mid+1;
-            }
-            else{
-                h = mid-1;
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+                max=Math.max(max,nums[i]);
+                return i;
             }
         }
-        return -1;
+        // int l = 1;
+        // int h = nums.length-2;
+        // while(l<=h){
+        //     int mid = l + (h-l)/2;
+        //     if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+        //         return mid;
+        //     }
+        //     if(nums[mid]>nums[mid-1]){
+        //         l = mid+1;
+        //     }
+        //     else{
+        //         h = mid-1;
+        //     }
+        // }
+        return 0;
     }
 }

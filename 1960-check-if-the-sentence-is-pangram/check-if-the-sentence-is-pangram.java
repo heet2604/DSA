a7f5292c8f8da59daba90fe10fafcbc054f1f354
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2025-02-15 09:04:39 +0530

    Time: 4 ms (29.36%) | Memory: 41.7 MB (45.22%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        Set set = new HashSet<>();
+        Set<Character> set = new HashSet<>();
         for(int i=0;i<sentence.length();i++){
             set.add(sentence.charAt(i));
         }

2d7643088774364fd64658e6512f6e744a9ec040
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2025-04-04 06:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (53.07%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -14,24 +14,40 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
+    // public boolean isSymmetric(TreeNode root) {
+    //     if(root==null) return true;
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.add(root.left);
+    //     q.add(root.right);
+
+    //     while(!q.isEmpty()){
+    //         TreeNode left = q.poll();
+    //         TreeNode right = q.poll();
+
+    //         if(left==null && right==null) continue;
+    //         if(left==null || right==null || left.val!=right.val) return false;
+
+    //         q.add(left.left);
+    //         q.add(right.right);
+    //         q.add(left.right);
+    //         q.add(right.left);
+    //     }
+    //     return true;
+    // }
+
+    public boolean isSymmetric(TreeNode root){
         if(root==null) return true;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root.left);
-        q.add(root.right);
 
-        while(!q.isEmpty()){
-            TreeNode left = q.poll();
-            TreeNode right = q.poll();
+        return Mirror(root.left,root.right);
+    }
 
-            if(left==null && right==null) continue;
-            if(left==null || right==null || left.val!=right.val) return false;
+    public boolean Mirror(TreeNode left,TreeNode right){
+        if(left==null && right==null) return true;
+        if(left==null || right==null) return false;
 
-            q.add(left.left);
-            q.add(right.right);
-            q.add(left.right);
-            q.add(right.left);
+        if(left.val==right.val){
+            return Mirror(left.left,right.right) && Mirror(left.right,right.left);
         }
-        return true;
+        return false;
     }
 }

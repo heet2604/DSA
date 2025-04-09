78c26f1877491af0bf78dd3de66713d76635af43
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2025-04-09 16:32:15 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (82.91%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,37 +1,53 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0;
-        int h=nums.length-1;
-        while(l<=h){
-            // if(nums[l]==target){
-            //     return l;
-            // }
-            // else if(nums[h]==target){
-            //     return h;
-            // }
-            // else{
-            //     l++;
-            //     h--;
-            // }
-            int mid = (l+h)/2;
-            if(nums[mid]==target){
+        int pivot = findpivot(nums);
+        if (pivot == -1) {
+            return binary(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return binary(nums, target, 0, pivot - 1);
+        }
+        return binary(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    public int findpivot(int[] nums) {
+        int s = 0;
+        int e = nums.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            // Case 1: mid > mid+1
+            if (mid < e && nums[mid] > nums[mid + 1]) {
                 return mid;
             }
-            if(nums[l]<=nums[mid]){
-                if(nums[l]<=target && target<=nums[mid]){
-                    h = mid-1;
-                }
-                else{
-                    l = mid+1;
-                }
+
+            // Case 2: mid < mid-1
+            if (mid > s && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
             }
-            else{
-                if(nums[mid]<=target && target<=nums[h]){
-                    l = mid+1;
-                }
-                else{
-                    h = mid-1;
-                }
+
+            // Case 3: left side is unsorted
+            if (nums[s] > nums[mid]) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int binary(int[] nums, int x, int s, int e) {
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == x) {
+                return mid;
+            } else if (nums[mid] > x) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
         return -1;

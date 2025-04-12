4448fd64b756f863a0137b5bfbd5684c9596905a
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2025-04-12 11:17:20 +0530

    Time: 6 ms (51.81%) | Memory: 58.1 MB (66.38%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -10,26 +10,44 @@ class Solution {
         //         }
         //     }
         // }
-        int l = 1;
-        int h = nums.length-1;
-        int dup = -1;
-        while(l<=h){
-            int mid = l + (h-l)/2;
-            int count = 0;
+        // int l = 1;
+        // int h = nums.length-1;
+        // int dup = -1;
+        // while(l<=h){
+        //     int mid = l + (h-l)/2;
+        //     int count = 0;
 
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]<=mid){
-                    count++;
-                }
-            }
-            if(count>mid){
-                dup = mid;
-                h = mid-1;
+        //     for(int i=0;i<nums.length;i++){
+        //         if(nums[i]<=mid){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>mid){
+        //         dup = mid;
+        //         h = mid-1;
+        //     }
+        //     else{
+        //         l = mid+1;
+        //     }
+        // }
+        // return dup; 
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i];
+            if(nums[i]!=nums[correct]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
             }
             else{
-                l = mid+1;
+                i++;
             }
         }
-        return dup; 
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j){
+                return nums[j];
+            }
+        }
+        return nums[nums.length-1];
     }
 }
